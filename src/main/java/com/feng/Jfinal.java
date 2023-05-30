dc3d6c6d5fc210a39ec2a6ceb75f5e866b088a6e
chunxue <chunxue@yikangsi.cn>
2023-05-30 10:25:02 +0800

    1

--- src/main/java/com/feng/Jfinal.java
+++ src/main/java/com/feng/Jfinal.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Jfinal {
     public Object jfinalInitMysqlDB() {
-        String url="jdbc:mysql://mysql.uat.kanghong.local:3306/ec_order_uat?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&failOverReadOnly=false&serverTimezone=Asia/Shanghai&zeroDateTimeBehavior=convertToNull&allowMultiQueries=true";
+        String url="jdbc:mysql://mysql.uat.1.local:3306/ec_order_uat?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&failOverReadOnly=false&serverTimezone=Asia/Shanghai&zeroDateTimeBehavior=convertToNull&allowMultiQueries=true";
         String username="chunxue";
         String password="h_4h0eV3s0kvuUVwhtbG";
         DruidPlugin druidPlugin = new DruidPlugin(url,username, password);
@@ -20,7 +20,7 @@ public class Jfinal {
         return new Object();
     }
     public Object jfinalInitMssqlDB() {
-        String url="jdbc:sqlserver://mssql.uat.kanghong.local;DatabaseName=Inspharmacy";
+        String url="jdbc:sqlserver://mssql.uat.1.local;DatabaseName=Inspharmacy";
         String username="ec_order_service_uat";
         String password="5v01Tk7xSnO_74CufjZ0i";
         DruidPlugin druidPlugin = new DruidPlugin(url,username, password);
@@ -50,6 +50,5 @@ public class Jfinal {
             System.out.println(name);
         }
         Record record=new Record();
-        Record r=new Record();
     }
 }
